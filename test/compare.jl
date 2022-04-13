a440b1a2facba3949e3d0622e67e270089332a40
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2022-04-13 01:11:49 -0400

    test compare

--- test/compare.jl
+++ test/compare.jl
@@ -11,4 +11,29 @@
 
     @test isequal(atimedate, atimedate)
     @test isless(earlier_timedate, later_timedate)
+
+    @test isequal(adate, atimedate) == false
+    @test isequal(TimeDate(adate), adate) == true
+    @test isequal(adatetime, atimedate) == false
+    @test isequal(TimeDate(adatetimed), adatetime) == true
+
+    @test isless(adate, atimedate) == true
+    @test isless(TimeDate(adate), adate) == false
+    @test isless(adatetime, atimedate) == true
+    @test isless(TimeDate(adatetimed), adatetime) == false
+
+    @test ==(adate, atimedate) == false
+    @test ==(TimeDate(adate), adate) == true
+    @test ==(adatetime, atimedate) == false
+    @test ==(TimeDate(adatetimed), adatetime) == true
+
+    @test <(adate, atimedate) == true
+    @test <(TimeDate(adate), adate) == false
+    @test <(adatetime, atimedate) == true
+    @test <(TimeDate(adatetimed), adatetime) == false
+
+    @test <=(adate, atimedate) == true
+    @test <=(TimeDate(adate), adate) == false
+    @test <=(adatetime, atimedate) == true
+    @test <=(TimeDate(adatetimed), adatetime) == false
 end
