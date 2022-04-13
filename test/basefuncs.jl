05e452be49148b6a249a4047072dd2a4df6f02d6
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2022-04-13 01:02:52 -0400

    test basefuncs

--- test/basefuncs.jl
+++ test/basefuncs.jl
@@ -1,3 +1,8 @@
 @testset "basefuncs" begin
-  @test true
+  @test iszero(Time(0)) == true
+  @test eps(atimedate) == Nanosecond(1)
+  @test DateTime(typemax(TimeDate)) == typemax(DateTime)
+  @test DateTime(typemin(TimeDate)) == typemin(DateTime)
+  @test repr(atimedate) == "TimeDate(\"2022-04-09T12:48:35.877012505\")"
+  @test hash(atimedate) == 0xf355e41d87cf2d89
 end
