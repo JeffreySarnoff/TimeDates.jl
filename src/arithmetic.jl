909cb7770fb8ec60defac1a02755cf8b56563d64
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2022-04-13 10:46:54 -0400

    arithmetic reworked

--- src/arithmetic.jl
+++ src/arithmetic.jl
@@ -1,36 +1,52 @@
-for (T, PerDay) in ((:Hour, HoursPerDay), (:Minute, MinutesPerDay), (:Second, SecondsPerDay), (:Millisecond, MillisecondsPerDay), (:Microsecond, MicrosecondsPerDay), (:Nanosecond, NanosecondsPerDay))
+for T in (:Hour, :Minute, :Second, :Millisecond, :Microsecond, :Nanosecond)
     @eval begin
         function Base.:(+)(td::TimeDate, x::$T)
-            yearnum = year(td)
-            date = Date(yearnum)
-            ndays = Dates.days(td) - Dates.days(date) + Dates.days(x)
-            nnanos = value(td.time) + Dates.tons(x)
-            time = Time(Nanosecond(nnanos))
-            date = date + Day(ndays)
-            TimeDate(time, date)
+            totalnanos = Dates.value(td) + Dates.value(x)
+            ndays, nnanos = divrem(totalnanos, NanosecondsPerDay)
+            TimeDate(rata2datetime(ndays)) + Nanosecond(nnanos)
         end
+
         Base.:(+)(x::$T, td::TimeDate) = td + x
+        
         function Base.:(-)(td::TimeDate, x::$T)
-            yearnum = year(td)
-            date = Date(yearnum)
-            ndays = Dates.days(td) - Dates.days(date) - Dates.days(x)
-            nnanos = value(td.time) - Dates.tons(x)
-            time = Time(Nanosecond(nnanos))
-            date = date + Day(ndays)
-            TimeDate(time, date)
-        end
-        function Base.:(-)(td::TimeDate, x::$T)
-            y = -x
-            td + y
+            totalnanos = Dates.value(td) - Dates.value(x)
+            ndays, nnanos = divrem(totalnanos, NanosecondsPerDay)
+            TimeDate(rata2datetime(ndays)) + Nanosecond(nnanos)
         end
     end
 end
 
-for F in (:Year, :Quarter, :Month, :Week, :Day)
+for F in (:Week, :Day)
     @eval begin
-        Base.:(+)(td::TimeDate, x::$F) = TimeDate(td.time, td.date + x)
-        Base.:(+)(x::$F, td::TimeDate) = td + x
-        Base.:(-)(td::TimeDate, x::$F) = TimeDate(td.time, td.date - x)
+        function Base.:(+)(td::TimeDate, x::$T)
+            totalnanos = Dates.value(td) + Dates.tons(x)
+            ndays, nnanos = divrem(totalnanos, NanosecondsPerDay)
+            TimeDate(rata2datetime(ndays)) + Nanosecond(nnanos)
+        end
+
+        Base.:(+)(x::$T, td::TimeDate) = td + x
+
+        function Base.:(-)(td::TimeDate, x::$T)
+            totalnanos = Dates.value(td) - Dates.tons(x)
+            ndays, nnanos = divrem(totalnanos, NanosecondsPerDay)
+            TimeDate(rata2datetime(ndays)) + Nanosecond(nnanos)
+        end
+
+    end
+end
+
+for F in (:Year, :Quarter, Month)
+    @eval begin
+        function Base.:(+)(td::TimeDate, x::$T)
+            TimeDate(td.time, td.date + x)
+        end
+
+        Base.:(+)(x::$T, td::TimeDate) = td + x
+
+        function Base.:(-)(td::TimeDate, x::$T)
+            TimeDate(td.time, td.date - x)
+        end
+
     end
 end
 
